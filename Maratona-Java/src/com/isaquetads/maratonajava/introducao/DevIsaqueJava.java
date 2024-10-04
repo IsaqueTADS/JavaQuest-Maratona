e601044284d6a63b81943c467c966e21c1a4e8d1
IsaqueTADS Ubuntu <isaqueofc18@gmail.com>
2024-10-04 17:06:45 -0300

    comentarios

--- Maratona-Java/src/com/isaquetads/maratonajava/introducao/DevIsaqueJava.java
+++ Maratona-Java/src/com/isaquetads/maratonajava/introducao/DevIsaqueJava.java
@@ -1,5 +1,11 @@
 package com.isaquetads.maratonajava.introducao;
+
 public class DevIsaqueJava {
+    /**
+     * 
+     * isso é um comentario legal <br>
+     * java doc </br>
+     */
     public static void main(String[] args) {
         System.out.println("Eu amo viver\nJesus traz sentido à vida\nTankyou jesus\nHello World");
 
