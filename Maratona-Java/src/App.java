7f999224c23f786fd835e9658b4f7a6e0396da94
IsaqueTADS Ubuntu <isaqueofc18@gmail.com>
2024-10-04 15:52:17 -0300

    primeiros passos no java

---
+++ Maratona-Java/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Queria dizer nada não,\nMas agora eu sou progamador em java tambem bbb v3");
+    }
+}
---
+++ Maratona-Java/src/com/isaquetads/maratonajava/introducao/DevIsaqueJava.java
@@ -0,0 +1,8 @@
+package com.isaquetads.maratonajava.introducao;
+public class DevIsaqueJava {
+    public static void main(String[] args) {
+        System.out.println("Eu amo viver\nJesus traz sentido à vida\nTankyou jesus\nHello World");
+
+    }
+
+}
